37a9324ec00fac4afdf3207f5489f9736bac0ff8
Dohival <67165702+Dohival@users.noreply.github.com>
2021-11-26 11:19:40 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8">
         <title></title>
         <link rel="stylesheet" href="./CSS/estilos.css">
+        <link rel="stylesheet" href="./CSS/estilosheader.css">
         <meta name="viewport" content="width=device-widht, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimun-scale=1.0">
         <link rel="stylesheet" href="./CSS/estilosformulario.css">
         <?php
@@ -214,59 +215,4 @@
 <?php
 include './Diseños/Footer.php';
 ?>
-<!-- <footer>
-     <div class="conteiner-footer-all">
-         <div class="container-body">
-             <div class="colum1">
-                 <h1>Sobre nosotros</h1>
-                 <div class="row">
-                     <a href="index.php"><label>¿Quiénes somos?</label></a>
-                 </div>
-                 <div class="row">
-                     <a href="index.php"><label>Fundadores</label></a>
-                 </div>
-             </div>
-             <div class="colum2">
-                 <h1>Contáctanos</h1>
-                 <div class="row">
-                     <a href="index.php"><img src="./img/mail_logo.png">
-                         <label>Mail:</label></a>
-                 </div>
-                 <div class="row">
-                     <a href="index.php"><img src="./img/whatsapp logo.png">
-                         <label>Whatsapp:</label></a>
-                 </div>
-             </div>
-             <div class="colum3">
-                 <h1>¡Síguenos!</h1>
-                 <div class="row2">
-                     <a href="https://www.facebook.com/ibgcorpoficial/">
-                         <img src="./img/facebook_logo.png">
-                         <label>Facebook</label></a>
-                 </div>
-                 <div class="row2">
-                     <a href="http://www.instagram.com">
-                         <img src="./img/instagram_logo.png">
-                         <label>Instagram</label></a>
-                 </div>
-                 <div class="row2">
-                     <a href="https://www.linkedin.com/company/corporacion-ibgroup/">
-                         <img src="./img/linkedin logo.png">
-                         <label>LinkedIn</label></a>
-                 </div>
-                 <div class="row2">
-                     <a href="https://twitter.com/IBGroupConsulto">
-                         <img src="./img/twitter logo.png">
-                         <label>Twitter</label></a>
-                 </div>
-             </div>
-         </div>
-         <div>
-             <div class="pieaño">
-                 <img src="">
-                 <label>© ibgroup 2021</label>
-             </div>
-         </div>
-     </div>
- </footer>-->
 </html>
