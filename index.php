020583317120189e7279dc70bc2d63c39a8da2ff
Dohival <67165702+Dohival@users.noreply.github.com>
2021-11-25 20:35:18 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -6,19 +6,22 @@
         <link rel="stylesheet" href="./CSS/estilos.css">
         <meta name="viewport" content="width=device-widht, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimun-scale=1.0">
         <link rel="stylesheet" href="./CSS/estilosformulario.css">
+        <?php
+        include './Diseños/Header.php';
+        ?>
     </head>
-        <header>
-            <div class="container-header-all">
-                <div class="logo-header">
-                    <a href="index.php"><img src="img/logoibcorp.png"></a>
-                </div>
-                <div class="menu-header">
-                    <a href="index.php"><label>Planes</label></a>
-                    <a href="index.php"><label>Sobre nosotros</label></a>
-                    <a href="#formulario"><label>Contacto</label></a>
-                </div>
-            </div>
-        </header>
+    <!--  <header>
+          <div class="container-header-all">
+              <div class="logo-header">
+                  <a href="index.php"><img src="img/logoibcorp.png"></a>
+              </div>
+              <div class="menu-header">
+                  <a href="index.php"><label>Planes</label></a>
+                  <a href="index.php"><label>Sobre nosotros</label></a>
+                  <a href="#formulario"><label>Contacto</label></a>
+              </div>
+          </div>
+      </header>-->
     <body>
         <section class="panel-inicio">
             <article>
@@ -208,59 +211,62 @@
             </div>
         </section>
 </body>
-        <footer>
-            <div class="conteiner-footer-all">
-                <div class="container-body">
-                    <div class="colum1">
-                        <h1>Sobre nosotros</h1>
-                        <div class="row">
-                            <a href="index.php"><label>¿Quiénes somos?</label></a>
-                        </div>
-                        <div class="row">
-                            <a href="index.php"><label>Fundadores</label></a>
-                        </div>
-                    </div>
-                    <div class="colum2">
-                        <h1>Contáctanos</h1>
-                        <div class="row">
-                            <a href="index.php"><img src="./img/mail_logo.png">
-                                <label>Mail:</label></a>
-                        </div>
-                        <div class="row">
-                            <a href="index.php"><img src="./img/whatsapp logo.png">
-                                <label>Whatsapp:</label></a>
-                        </div>
-                    </div>
-                    <div class="colum3">
-                        <h1>¡Síguenos!</h1>
-                        <div class="row2">
-                            <a href="https://www.facebook.com/ibgcorpoficial/">
-                                <img src="./img/facebook_logo.png">
-                                <label>Facebook</label></a>
-                        </div>
-                        <div class="row2">
-                            <a href="http://www.instagram.com">
-                                <img src="./img/instagram_logo.png">
-                                <label>Instagram</label></a>
-                        </div>
-                        <div class="row2">
-                            <a href="https://www.linkedin.com/company/corporacion-ibgroup/">
-                                <img src="./img/linkedin logo.png">
-                                <label>LinkedIn</label></a>
-                        </div>
-                        <div class="row2">
-                            <a href="https://twitter.com/IBGroupConsulto">
-                                <img src="./img/twitter logo.png">
-                                <label>Twitter</label></a>
-                        </div>
-                    </div>
-                </div>
-                <div>
-                    <div class="pieaño">
-                        <img src="">
-                        <label>© ibgroup 2021</label>
-                    </div>
-                </div>
-            </div>
-        </footer>
+<?php
+include './Diseños/Footer.php';
+?>
+<!-- <footer>
+     <div class="conteiner-footer-all">
+         <div class="container-body">
+             <div class="colum1">
+                 <h1>Sobre nosotros</h1>
+                 <div class="row">
+                     <a href="index.php"><label>¿Quiénes somos?</label></a>
+                 </div>
+                 <div class="row">
+                     <a href="index.php"><label>Fundadores</label></a>
+                 </div>
+             </div>
+             <div class="colum2">
+                 <h1>Contáctanos</h1>
+                 <div class="row">
+                     <a href="index.php"><img src="./img/mail_logo.png">
+                         <label>Mail:</label></a>
+                 </div>
+                 <div class="row">
+                     <a href="index.php"><img src="./img/whatsapp logo.png">
+                         <label>Whatsapp:</label></a>
+                 </div>
+             </div>
+             <div class="colum3">
+                 <h1>¡Síguenos!</h1>
+                 <div class="row2">
+                     <a href="https://www.facebook.com/ibgcorpoficial/">
+                         <img src="./img/facebook_logo.png">
+                         <label>Facebook</label></a>
+                 </div>
+                 <div class="row2">
+                     <a href="http://www.instagram.com">
+                         <img src="./img/instagram_logo.png">
+                         <label>Instagram</label></a>
+                 </div>
+                 <div class="row2">
+                     <a href="https://www.linkedin.com/company/corporacion-ibgroup/">
+                         <img src="./img/linkedin logo.png">
+                         <label>LinkedIn</label></a>
+                 </div>
+                 <div class="row2">
+                     <a href="https://twitter.com/IBGroupConsulto">
+                         <img src="./img/twitter logo.png">
+                         <label>Twitter</label></a>
+                 </div>
+             </div>
+         </div>
+         <div>
+             <div class="pieaño">
+                 <img src="">
+                 <label>© ibgroup 2021</label>
+             </div>
+         </div>
+     </div>
+ </footer>-->
 </html>
