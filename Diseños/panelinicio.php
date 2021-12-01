5cb738f5986a4e2895a1a5e417bd0935fb824c3c
luis20211 <94995535+luis20211@users.noreply.github.com>
2021-12-01 17:13:52 -0500

    Add files via upload

---
+++ Diseños/panelinicio.php
@@ -0,0 +1,27 @@
+<section name="panel-inicio"> 
+    <div class="container-panel-inicio-all">
+    <div class="container-body">
+                    <div class="colum1">
+                    <div class="row">
+                        <h1>POTENCIA TU CLIMA Y CULTURA LABORAL CON LA TECNOLOGIA Y LO EVENTOS</h1>
+                        </div>
+                        <div class="row">
+                            <a href="index.php">ORGANIZA ENVENTOS VIRTUALES PARA
+                                 INTEGRAR , MOTIVAR Y EMPODERAR ALOS TRABAJADORES</a>
+                        </div>
+                    </div>
+
+    </div>
+    <div class = "container-img">
+        <div class="row">
+            <img src ="./img/imagen.jpg">
+        </div>
+    </div>
+        <div" class= "container-img">
+        <div class="row">
+           <img src ="./img/imagen.jpg">
+           </div>
+        </div>
+</section>
+
+    
