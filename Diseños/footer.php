ddcb84f93dfd3f946c3b60059b13b07be0450a76
Dohival <67165702+Dohival@users.noreply.github.com>
2021-12-02 03:15:48 -0500

    Add files via upload

---
+++ Diseños/footer.php
@@ -0,0 +1,53 @@
+
+            <div class="conteiner-footer-all">
+                <div class="container-body">
+                    <div class="colum1">
+                        <h1>Sobre nosotros</h1>
+                        <div class="row">
+                            <a href="index.php"><label>¿Quiénes somos?</label></a>
+                        </div>
+                        <div class="row">
+                            <a href="index.php"><label>Fundadores</label></a>
+                        </div>
+                    </div>
+                    <div class="colum2">
+                        <h1>Contáctanos</h1>
+                        <div class="row">
+                            <a href="mailto:cm.outplacement.coaching@corpibgroup.com"><img src="./img/mail_logo.png">
+                                <span>Mail: cm.outplacement.coaching@corpibgroup.com</span></a>
+                        </div>
+                        <div class="row">
+                            <a href="https://wa.me/51983481263"><img src="./img/whatsapp logo.png">
+                                <span>Whatsapp: +51 983 481 263</span></a>
+                        </div>
+                    </div>
+                    <div class="colum3">
+                        <h1>¡Síguenos!</h1>
+                        <div class="row2">
+                            <a href="https://www.facebook.com/IBOutplacementOficial/">
+                                <img src="https://storage.googleapis.com/ebombo-events.appspot.com/resources/facebook-gray.svg">
+                                Facebook</a>
+                        </div>
+                        <div class="row2">
+                            <a href="https://www.instagram.com/iboutplacement/">
+                                <img src="./img/instagram_logo.png">
+                                Instagram</a>
+                        </div>
+                        <div class="row2">
+                            <a href="https://www.linkedin.com/company/iboutplacement">
+                                <img src="./img/linkedin logo.png">
+                                LinkedIn</a>
+                        </div>
+                        <div class="row2">
+                            <a href="https://twitter.com/ib_outplacement">
+                                <img src="./img/twitter logo.png">
+                                Twitter</a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="pieaño">
+                           <label>© ibgroup 2021</label>
+                    </div>
+                </div>
+            </div>
---
+++ Diseños/header.php
@@ -0,0 +1,36 @@
+  <header>
+  <nav class="navbar">
+    <a href="index.php"><img src="img/logoibcorp.png"></a>
+      <ul class="nav-menu">
+        <li class="nav-item">
+          <a href="index.php" class="nav-link">Planes</a>
+        </li>
+        <li class="nav-item1">
+          <a href="index.php" class="nav-link">Sobre Nosotros</a>
+        </li>
+        <li class="nav-item">
+          <a href="#formulario" class="nav-link">Contacto</a>
+        </li>
+      </ul>
+      <div class="hamburger">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span>
+      </div>
+    </nav>
+    <script>
+        const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+        </script>
+
+  </header>
