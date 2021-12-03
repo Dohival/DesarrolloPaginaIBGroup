bd794dd3e6e4f57f198c62e97bc2d884596470b4
Dohival <67165702+Dohival@users.noreply.github.com>
2021-12-03 14:20:32 -0500

    Add files via upload

--- Diseños/email.php
+++ Diseños/email.php
@@ -1,15 +1,21 @@
 <?php
-$myemail = 'osvaldo_528@outlook.com';
+if(isset($_POST['Enviar']))
+{
+    if(!empty($_POST['nombre']) && !empty($_POST['apellido']) && !empty($_POST['correo']) && !empty($_POST['empresa']) && !empty($_POST['telefono']) && !empty($_POST['consulta'])){
+
 $name = $_POST['nombre'];
 $surname = $_POST['apellido'];
 $email = $_POST['correo'];
 $company = $_POST['empresa'];
 $phone = $_POST['telefono'];
 $message = $_POST['consulta'];
-$
-$to = $myemail;
-$email_subject = "Nuevo mensaje: $subject";
-$email_body = "Haz recibido un nuevo mensaje. \n Nombre: $name \n Apellido: $surname \n Correo: $email \n Empresa: $company \n Telefono: $phone \n Mensaje: \n $message";
-$headers = "From: $email";
-mail($to, $email_subject, $email_body, $headers);
-echo "El mensaje se ha enviado correctamente";
+
+$header="From: noreply@example.com"."\r\n";
+$header.="Reaply-To: noreply@example.com"."\r\n";
+$header.="X-Mailer: PHP/".phpversion();
+$mail=@mail($email,"Asunto Prueba",$message, $header);
+if($mail){
+    echo "<h4>Mail enviado</h4>";
+}
+}
+}
--- Diseños/footer.php
+++ Diseños/footer.php
@@ -1,27 +1,27 @@
 
             <div class="conteiner-footer-all">
                 <div class="container-body">
-                    <div class="colum1">
+                    <div class="colu1">
                         <h1>Sobre nosotros</h1>
                         <div class="row">
-                            <a href="index.php"><label>¿Quiénes somos?</label></a>
+                            <a href="index.php">¿Quiénes somos?</a>
                         </div>
                         <div class="row">
-                            <a href="index.php"><label>Fundadores</label></a>
+                            <a href="index.php">Fundadores</a>
                         </div>
                     </div>
-                    <div class="colum2">
+                    <div class="colu2">
                         <h1>Contáctanos</h1>
                         <div class="row">
                             <a href="mailto:cm.outplacement.coaching@corpibgroup.com"><img src="./img/mail_logo.png">
-                                <span>Mail: cm.outplacement.coaching@corpibgroup.com</span></a>
+                                Mail: cm.outplacement.coaching@corpibgroup.com</a>
                         </div>
                         <div class="row">
                             <a href="https://wa.me/51983481263"><img src="./img/whatsapp logo.png">
-                                <span>Whatsapp: +51 983 481 263</span></a>
+                                Whatsapp: +51 983 481 263</a>
                         </div>
                     </div>
-                    <div class="colum3">
+                    <div class="colu3">
                         <h1>¡Síguenos!</h1>
                         <div class="row2">
                             <a href="https://www.facebook.com/IBOutplacementOficial/">
