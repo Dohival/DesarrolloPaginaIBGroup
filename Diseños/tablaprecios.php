22059f6c0d69d127ba4bbafe51c3a8f1339a2fb9
Dohival <67165702+Dohival@users.noreply.github.com>
2021-12-03 19:37:12 -0500

    Add files via upload

---
+++ Diseños/tablaprecios.php
@@ -0,0 +1,24 @@
+<div class="titulo"><h1>Conoce nuestros planes</h1></div>
+<section class="contenedor">
+        <div class="tabla">
+            <h2>Básico</h2>
+            <img src="png/1.png" alt="">
+            <h3>200 <sup>$</sup></h3>
+            <p>Paga ahora y recibe un 30% en tu hosting y dominios</p>
+            <a href="" class="boton">Paga ahora</a>
+        </div>
+        <div class="tabla hover">
+            <h2>Estandar</h2>
+            <img src="png/2.png" alt="">
+            <h3>200 <sup>$</sup></h3>
+            <p>Paga ahora y recibe un 30% en tu hosting y dominios</p>
+            <a href="" class="boton">Paga ahora</a>
+        </div>
+        <div class="tabla">
+            <h2>Premium</h2>
+            <img src="png/3.png" alt="">
+            <h3>300 <sup>$</sup></h3>
+            <p>Paga ahora y recibe un 30% en tu hosting y dominios</p>
+            <a href="" class="boton">Paga ahora</a>
+        </div>
+</section>
