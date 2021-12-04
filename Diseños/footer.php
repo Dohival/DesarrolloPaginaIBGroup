2c988327c666f7c656ce567ba72bc33540fbc480
Dohival <67165702+Dohival@users.noreply.github.com>
2021-12-03 19:34:07 -0500

    Add files via upload

---
+++ Diseños/footer.php
@@ -0,0 +1,53 @@
+
+            <div class="conteiner-footer-all">
+                <div class="container-body">
+                    <div class="colu1">
+                        <h1>Sobre nosotros</h1>
+                        <div class="row">
+                            <a href="index.php">¿Quiénes somos?</a>
+                        </div>
+                        <div class="row">
+                            <a href="index.php">Fundadores</a>
+                        </div>
+                    </div>
+                    <div class="colu2">
+                        <h1>Contáctanos</h1>
+                        <div class="row">
+                            <a href="mailto:cm.outplacement.coaching@corpibgroup.com"><div class="row2"><img src="./img/mail_logo.png">
+                                Mail: cm.outplacement.coaching@corpibgroup.com</div></a>
+                        </div>
+                        <div class="row">
+                            <a href="https://wa.me/51983481263"><div class="row2"><img src="./img/whatsapp logo.png">
+                                Whatsapp: +51 983 481 263</div></a>
+                        </div>
+                    </div>
+                    <div class="colu3">
+                        <h1>¡Síguenos!</h1>
+                        <div class="row2">
+                            <a href="https://www.facebook.com/IBOutplacementOficial/">
+                                <div class="row2"><img src="https://storage.googleapis.com/ebombo-events.appspot.com/resources/facebook-gray.svg">
+                                Facebook</div></a>
+                        </div>
+                        <div class="row2">
+                            <a href="https://www.instagram.com/iboutplacement/">
+                                <div class="row2"><img src="./img/instagram_logo.png">
+                                Instagram</div></a>
+                        </div>
+                        <div class="row2">
+                            <a href="https://www.linkedin.com/company/iboutplacement">
+                                <div class="row2"><img src="./img/linkedin logo.png">
+                                LinkedIn</div></a>
+                        </div>
+                        <div class="row2">
+                            <a href="https://twitter.com/ib_outplacement">
+                                <div class="row2"><img src="./img/twitter logo.png">
+                                Twitter</div></a>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <div class="pieaño">
+                           <label>© ibgroup 2021</label>
+                    </div>
+                </div>
+            </div>
