6776aa82183146af3b7608d8343ff7c55fb6a4d3
Dohival <67165702+Dohival@users.noreply.github.com>
2021-12-03 19:38:50 -0500

    Add files via upload

---
+++ pagN1.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./CSS/estilosheader.css">
+        <link rel="stylesheet" href="./CSS/estilosfooter.css">
+        <link rel="stylesheet" href="./CSS/estilosformulario.css">
+        <link rel="stylesheet" href="./CSS/estilospaginaservicio.css">
+        <?php
+        include './Diseños/header.php';
+        ?>
+    </head>
+    <body>
+        <div class="container-paginaindividual">
+            <div class="container-descripción">
+                <h1>Agregar Titulo</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam maxime nisi modi, quod officia soluta illum magni error vero ex distinctio cupiditate minus aut animi fugit a unde, sed iusto.</p>
+                <ul class="normal">
+                <li>eeeeee</li>
+                <li>ffffff</li>
+                <li>dddddd</li>
+                <li>eeeeee</li>
+                </ul>
+                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni minima molestiae quo ipsam maxime nisi saepe rerum beatae labore ad sint, dignissimos, doloremque nesciunt. Repellat mollitia saepe voluptatem commodi distinctio.</p>
+            </div>
+            <div class="img-paginaindividual">
+                <img src="./img/A.png">
+            </div>
+            
+        </div>
+
+ <?php
+ include './Diseños/formulario.php';
+ ?>
+    </body>
+    <?php
+    include './Diseños/footer.php';
+    ?>
+</html>
